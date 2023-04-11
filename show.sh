ba034d6d2b8e599da973c1089558675f1fe527c1
rgblab <rgblab@rgblab>
2023-04-11 10:18:09 +0900

    add tracker_reshape... flatten layer -> reshape layer

--- show.sh
+++ show.sh
@@ -1,5 +1,6 @@
 # DEPLOY_PROTO='./model/tracker.prototxt'
-# CAFFE_MODEL='./model/tracker.caffemodel'
+DEPLOY_PROTO='./model/tracker_reshape.prototxt'
+CAFFE_MODEL='./model/tracker.caffemodel'
 
 # DEPLOY_PROTO='./model/myTracker.prototxt'
 # CAFFE_MODEL='./backup/my2Dump_model_iter_55000.caffemodel'
@@ -10,8 +11,8 @@
 # DEPLOY_PROTO='./model/tiny_v2/tiny2Tracker_deploy.prototxt'
 # CAFFE_MODEL='./tiny2Dump_model_iter_300000.caffemodel'
 
-DEPLOY_PROTO='./model/tiny_v3/tiny3Tracker_deploy.prototxt'
-CAFFE_MODEL='./tiny3Dump_model_iter_66000.caffemodel'
+# DEPLOY_PROTO='./model/tiny_v3/tiny3Tracker_deploy.prototxt'
+# CAFFE_MODEL='./tiny3Dump_model_iter_66000.caffemodel'
 
 TEST_DATA_PATH='/home/rgblab/tracking/dataset/VOT'
 
